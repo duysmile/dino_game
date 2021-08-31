75d653c2bf4d0284c5f2ceb93a0be845a9b5e9d8
duy.nguyen <duy210697@gmail.com>
2021-08-31 19:03:59 +0700

    [dev] update dialog plugin with text

--- src/plugins/dialog.js
+++ src/plugins/dialog.js
@@ -5,11 +5,12 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         super(pluginManager);
     }
 
-    init(opts) {
+    run(opts) {
         if (!opts) {
             opts = {};
         }
 
+        this.currentScene = this.game.scene.scenes[0];
         this.borderThickness = opts.borderThickness || 3;
         this.borderColor = opts.borderColor || 0x907748;
         this.borderAlpha = opts.borderAlpha || 1;
@@ -17,7 +18,7 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         this.windowColor = opts.windowColor || 0x303030;
         this.windowHeight = opts.windowHeight || 150;
         this.padding = opts.padding || 32;
-        this.closeBtnGroup = opts.closeBtnGroup || 'darkgoldenrod';
+        this.closeBtnColor = opts.closeBtnColor || 'darkgoldenrod';
         this.dialogSpeed = opts.dialogSpeed || 3;
 
         this.eventCounter = 0;
@@ -58,13 +59,103 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         this.graphics.strokeRect(x, y, rectWidth, rectHeight);
     }
 
+    _createCloseModalButton() {
+        this.closeBtn = this.currentScene.make.text({
+            x: this._getGameWidth() - this.padding - 14,
+            y: this._getGameHeight() - this.windowHeight - this.padding + 3,
+            text: 'X',
+            style: {
+                font: 'bold 12px Arial',
+                fill: this.closeBtnColor,
+            },
+        });
+
+        this.closeBtn.setInteractive();
+        this.closeBtn.on('pointerover', () => {
+            this.closeBtn.setTint(0xff0000);
+        });
+
+        this.closeBtn.on('pointerout', () => {
+            this.closeBtn.clearTint();
+        });
+
+        this.closeBtn.on('pointerdown', () => {
+            this.toggleWindow();
+        });
+    }
+
+    _createCloseModalButtonBorder() {
+        const x = this._getGameWidth() - this.padding - 20;
+        const y = this._getGameHeight() - this.windowHeight - this.padding;
+
+        this.graphics.strokeRect(x, y, 20, 20);
+    }
+
+    toggleWindow() {
+        this.visible = !this.visible;
+        if (this.timedEvent) this.timedEvent.remove();
+        if (this.text) {
+            this.text.visible = this.visible;
+            this.text.destroy();
+        }
+        if (this.graphics) this.graphics.visible = this.visible;
+        if (this.closeBtn) this.closeBtn.visible = this.visible;
+    }
+
+    setText(text, animate) {
+        this.eventCounter = 0;
+        this.dialog = text.split('');
+
+        if (this.timedEvent) this.timedEvent.remove();
+
+        let tempText = animate ? '' : text;
+
+        this._setText(tempText);
+        if (animate) {
+            this.timedEvent = this.currentScene.time.addEvent({
+                delay: 150 - this.dialogSpeed * 30,
+                callback: this._animateText,
+                callbackScope: this,
+                loop: true,
+            });
+        }
+    }
+
+    _animateText() {
+        this.eventCounter++;
+        this.text.setText(this.text.text + this.dialog[this.eventCounter - 1]);
+        if (this.eventCounter === this.dialog.length) {
+            this.timedEvent.remove();
+        }
+    }
+
+    _setText(text) {
+        if (this.text) this.text.destroy();
+
+        const x = this.padding + 10;
+        const y = this._getGameHeight() - this.windowHeight - this.padding + 10;
+
+        this.text = this.currentScene.make.text({
+            x,
+            y,
+            text,
+            style: {
+                wordWrap: {
+                    width: this._getGameWidth() - this.padding * 2 - 25,
+                },
+            },
+        });
+    }
+
     _createWindow() {
         const gameWidth = this._getGameWidth();
         const gameHeight = this._getGameHeight();
         const dimensions = this._calculateWindowDimensions(gameWidth, gameHeight);
-        this.graphics = this.game.scene.scenes[0].add.graphics();
+        this.graphics = this.currentScene.add.graphics();
 
         this._createOuterWindow(dimensions.x, dimensions.y, dimensions.rectWidth, dimensions.rectHeight);
         this._createInnerWindow(dimensions.x, dimensions.y, dimensions.rectWidth, dimensions.rectHeight);
+        this._createCloseModalButton();
+        this._createCloseModalButtonBorder();
     }
 }
--- src/typewriter.js
+++ src/typewriter.js
@@ -6,37 +6,14 @@ export default class Typewriter extends Phaser.Scene {
         super();
     }
 
-    preload() {
-    }
+    preload() {}
 
     create() {
         const dialogModalPlugin = this.plugins.install('dialogModal', DialogPlugin, true);
-        console.log(dialogModalPlugin);
 
-        dialogModalPlugin.init();
-
-        this.label = this.add.text(100, 100, '').setWordWrapWidth(500);
-        this.typewriteTextWrapped("Xin chào anh em thiện lành, chúng mình hoàn thành đươc một phần nghìn của game rồi nè!");
+        dialogModalPlugin.run();
+        dialogModalPlugin.setText("Hello world", true);
     }
 
-    typewriteText(text) {
-        const length = text.length;
-
-        let i = 0;
-        this.time.addEvent({
-            callback: () => {
-                this.label.text += text[i];
-                i++;
-            },
-            repeat: length - 1,
-            delay: 50,
-        });
-    }
-
-    typewriteTextWrapped(text) {
-        const lines = this.label.getWrappedText(text);
-        const wrappedText = lines.join('\n');
-
-        this.typewriteText(wrappedText);
-    }
+    update() {}
 }
