5037125dcd5c5ec137327ba13b64fd15863276ce
duy.nguyen <duy210697@gmail.com>
2021-09-05 02:43:47 +0700

    [dev] add confirm feature

--- src/plugins/dialog.js
+++ src/plugins/dialog.js
@@ -1,5 +1,7 @@
 import Phaser from "phaser";
 
+const UP = -1;
+const DOWN = 1;
 export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
     constructor(pluginManager) {
         super(pluginManager);
@@ -7,7 +9,7 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
 
     run(opts) {
         if (!opts) {
-            opts = {};
+            opts = { };
         }
 
         this.currentScene = this.game.scene.scenes[0];
@@ -18,6 +20,7 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         this.windowColor = opts.windowColor || 0x303030;
         this.windowHeight = opts.windowHeight || 150;
         this.padding = opts.padding || 32;
+        this.spaceSize = opts.spaceSize || 10;
         this.closeBtnColor = opts.closeBtnColor || 'darkgoldenrod';
         this.dialogSpeed = opts.dialogSpeed || 3;
 
@@ -92,43 +95,110 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         this.graphics.strokeRect(x, y, 20, 20);
     }
 
+    _clearObject(object) {
+        if (object) {
+            object.visible = this.visible;
+            object.destroy();
+        }
+    }
+
     toggleWindow() {
         this.visible = !this.visible;
         if (this.timedEvent) this.timedEvent.remove();
-        if (this.text) {
-            this.text.visible = this.visible;
-            this.text.destroy();
+        this._clearObject(this.text);
+
+        this._clearObject(this.instructionText);
+
+        if (this.choiceText) {
+            this.choiceText.forEach(this._clearObject.bind(this));
         }
+
+        this._clearObject(this.choicePointer);
+
         if (this.graphics) this.graphics.visible = this.visible;
         if (this.closeBtn) this.closeBtn.visible = this.visible;
     }
 
-    setText(text, animate) {
-        this.animate = animate;
+    setText(text, choices) {
         this.eventCounter = 0;
         this.dialog = text.split('');
+        this.choices = choices;
 
         if (this.timedEvent) this.timedEvent.remove();
 
-        this.isRunningAnimate = animate;
-        let tempText = animate ? '' : text;
+        this.isRunningAnimate = true;
 
-        this._setText(tempText);
-        if (animate) {
-            this.timedEvent = this.currentScene.time.addEvent({
-                delay: 100 - this.dialogSpeed * 30,
-                callback: this._animateText,
-                callbackScope: this,
-                loop: true,
-            });
-        }
+        this._setText('');
+        this._setInstructionText();
+        this.timedEvent = this.currentScene.time.addEvent({
+            delay: 100 - this.dialogSpeed * 30,
+            callback: this._animateText,
+            callbackScope: this,
+            loop: true,
+        });
     }
 
-    continueText() {
-        if (!this.animate) return;
+    _setInstructionText() {
+        this.instructionText = this.currentScene.make.text({
+            x: this._getGameWidth() - this.padding - 50,
+            y: this._getGameHeight() - this.padding - 20,
+            text: 'Press Z',
+            style: {
+                font: 'bold 12px Arial',
+                fill: this.closeBtnColor,
+            },
+        });
+    }
 
+    _setChoices() {
+        this.isRunningAnimate = true;
+        this.isShowChoice = true;
+        this.answer = 0;
+
+        const x = this.padding + 30;
+        let y = this._getGameHeight() - this.windowHeight - this.padding + this.spaceSize;
+        y += this.text.getBounds().height + this.spaceSize;
+
+        let lastHeight = 0;
+        this._initializePointer(x - 15, y + this.spaceSize);
+        this.choiceText = this.choices.map(choice => {
+            const temp = this.currentScene.make.text({
+                x,
+                y: y + lastHeight,
+                text: choice,
+                style: {
+                    font: "20px",
+                    wordWrap: {
+                        width: this._getGameWidth() - this.padding * 2 - 25,
+                    },
+                },
+            });
+
+            lastHeight = temp.getBounds().height + this.spaceSize;
+            return temp;
+        });
+    }
+
+    _setAnswer(direction) {
+        const oldX = this.choicePointer.x;
+        const oldY = this.choicePointer.y;
+
+        let step = direction * this.choiceText[this.answer - direction].getBounds().height;
+        step += direction * this.spaceSize;
+
+        this.choicePointer.setPosition(oldX, oldY + step);
+    }
+
+    _initializePointer(x, y) {
+		if (this.choicePointer != null) {
+			this.choicePointer.destroy();
+		}
+		this.choicePointer = this.currentScene.add.polygon(x, y, [0, 0, 0, 20, 10, 10], 0xffffff);
+	}
+
+    continueText() {
         const isEndOfText = this.eventCounter === this.dialog.length;
-        if (isEndOfText) {
+        if (isEndOfText && !this.isRunningAnimate) {
             return this.toggleWindow();
         }
 
@@ -150,7 +220,6 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         this.eventCounter++;
         const nextCharacter = this.dialog[this.eventCounter - 1];
         this.text.setText(this.text.text + nextCharacter);
-        console.log(this.text.getBounds().height);
 
         const isEndOfText = this.eventCounter === this.dialog.length;
         const isReachDot = nextCharacter === ".";
@@ -161,13 +230,17 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
             this.isRunningAnimate = false;
             this.timedEvent.remove();
         }
+
+        if (isEndOfText && this.choices) {
+            this._setChoices();
+        }
     }
 
     _setText(text) {
         if (this.text) this.text.destroy();
 
-        const x = this.padding + 10;
-        const y = this._getGameHeight() - this.windowHeight - this.padding + 10;
+        const x = this.padding + this.spaceSize;
+        const y = this._getGameHeight() - this.windowHeight - this.padding + this.spaceSize;
 
         this.text = this.currentScene.make.text({
             x,
@@ -190,6 +263,12 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         return this.visible;
     }
 
+    getAnswer() {
+        if (this.choices) {
+            return this.choices[this.answer];
+        }
+    }
+
     _createWindow() {
         const gameWidth = this._getGameWidth();
         const gameHeight = this._getGameHeight();
@@ -200,5 +279,37 @@ export default class DialogPlugin extends Phaser.Plugins.BasePlugin {
         this._createInnerWindow(dimensions.x, dimensions.y, dimensions.rectWidth, dimensions.rectHeight);
         this._createCloseModalButton();
         this._createCloseModalButtonBorder();
+    }
+
+    runEvent(keys) {
+        if (!this.checkExist()) {
+            return;
+        }
+        if (
+            this.checkExist()
+            && !this.checkRun()
+            && keys.z.isDown
+        ) {
+            this.continueText();
+        }
+
+        if (!this.isShowChoice) {
+            return
+        }
+
+        if (keys.enter.isDown) {
+            return this.toggleWindow();
+        }
+
+        if (keys.down.isDown || keys.up.isDown) {
+            let direction = keys.down.isDown ? DOWN : UP;
+            const temp = this.answer + direction;
+            if (temp < 0 || temp > this.choices.length - 1) {
+                return;
+            }
+            this.answer = temp;
+
+            this._setAnswer(direction);
+        }
     }
 }
--- src/typewriter.js
+++ src/typewriter.js
@@ -12,12 +12,25 @@ export default class Typewriter extends Phaser.Scene {
         this.dialogModalPlugin = this.plugins.install('dialogModal', DialogPlugin, true);
 
         this.dialogModalPlugin.run();
-        this.dialogModalPlugin.setText("AETL studio chính thức ra mắt và đạt được doanh thu hàng nghìn tỉ đồng từ game đầu tay, LOCKED ROOM. AETL studio chính thức ra mắt và đạt được doanh thu hàng nghìn tỉ đồng từ game đầu tay, LOCKED ROOM. AETL studio chính thức ra mắt và đạt được doanh thu hàng nghìn tỉ đồng từ game đầu tay, LOCKED ROOM.", true);
 
-        const { X, Z } = Phaser.Input.Keyboard.KeyCodes;
+        const text = "AETL studio chính thức ra mắt và đạt được doanh thu hàng nghìn tỉ đồng từ game đầu tay, LOCKED ROOM. AETL studio chính thức ra mắt và đạt được doanh thu hàng nghìn tỉ đồng từ game đầu tay, LOCKED ROOM. AETL studio chính thức ra mắt và đạt được doanh thu hàng nghìn tỉ đồng từ game đầu tay, LOCKED ROOM.";
+        const choices = [
+            "OK ngon",
+            "Not ok but ngon",
+        ];
+        this.dialogModalPlugin.setText(
+            text,
+            choices,
+            true,
+        );
+
+        const { X, Z, UP, DOWN, ENTER } = Phaser.Input.Keyboard.KeyCodes;
         this.keys = this.input.keyboard.addKeys({
             x: X,
             z: Z,
+            up: UP,
+            down: DOWN,
+            enter: ENTER,
         });
 
     }
@@ -25,12 +38,10 @@ export default class Typewriter extends Phaser.Scene {
     update() {
         const keys = this.keys;
 
-        if (
-            this.dialogModalPlugin.checkExist()
-            && !this.dialogModalPlugin.checkRun()
-            && keys.z.isDown
-        ) {
-            this.dialogModalPlugin.continueText();
+        this.dialogModalPlugin.runEvent(keys);
+
+        if (keys.enter.isDown && !this.dialogModalPlugin.checkExist()) {
+            console.log(this.dialogModalPlugin.getAnswer());
         }
     }
 }
